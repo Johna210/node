cd841ee6f7db2e42425aca44d13c22c8386c58a5
Johna210 <solomonjohna21@gmail.com>
2024-07-01 18:09:47 +0300

    initialized npm

--- node_js_server/simple_server.js
+++ node_js_server/simple_server.js
@@ -1,12 +1,12 @@
 const http = require("http");
 
 function rqListener(req, res) {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h1>Hello from Node</h1></body>");
-    res.write("</html>");
-    res.end();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from Node</h1></body>");
+  res.write("</html>");
+  res.end();
 }
 
 // http.createServer((req, res) => {});
