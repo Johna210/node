6109a270f63dd6cf101439bedc5bc82b3a1e60d1
Johna210 <solomonjohna21@gmail.com>
2024-07-08 16:59:14 +0300

    Implemented simple ejs template

--- 03_template_engines/app.js
+++ 03_template_engines/app.js
@@ -2,13 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const handlebars = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', handlebars.engine);
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.set('views', 'views/ejs');
 
 const rootDir = require('./util/path');
 
@@ -22,7 +20,7 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { docTitle: 'Page Not Found' });
+  res.status(404).render('404', { docTitle: 'Page Not Found', path: '404' });
 });
 
 app.listen(3000);
