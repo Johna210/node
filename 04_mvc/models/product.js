8250372664c8eaebf9b2601313995ed928932f98
Johna210 <solomonjohna21@gmail.com>
2024-07-08 22:09:33 +0300

    Simple MVC

---
+++ 04_mvc/models/product.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+const p = path.join(rootDir, 'data', 'products.json');
+
+function getProductsFromFile(callBack) {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      callBack([]);
+    } else {
+      callBack(JSON.parse(fileContent));
+    }
+  });
+}
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(callBack) {
+    getProductsFromFile(callBack);
+  }
+};
